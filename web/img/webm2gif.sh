51ed527f0740c9587f8080b8102770be69c51dba
Matias Lindgren <matias.lindgren@gmail.com>
2020-11-17 21:55:13 +0200

    move gif converter script

--- web/img/webm2gif.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-input=$1
-ffmpeg -i $input -vf "fps=10,scale=300:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 $(basename -s .webm $input).gif
